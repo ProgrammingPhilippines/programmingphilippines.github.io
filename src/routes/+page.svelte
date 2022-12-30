20db57227fe9928a49689dccdcabf1198f7665f6
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:16:43 +0800

    add: hero and about-us page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import AboutUs from '$lib/components/home/AboutUs.svelte';
+	import Hero from '$lib/components/home/Hero.svelte';
+</script>
+
+<Hero />
+<AboutUs />
