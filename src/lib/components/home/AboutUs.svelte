d8e3012890eb17b7fb6d0e2a33f354c05238d53c
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:16:59 +0800

    add: components for home page

---
+++ src/lib/components/home/AboutUs.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import UserCard from '../cards/UserCard.svelte';
+	import { adminsConfig } from '$lib/config/admins';
+	import { usersConfig } from '$lib/config/users';
+	import Member from '../cards/Member.svelte';
+</script>
+
+<div id="about-us" class="cotainer m-auto min-h-screen">
+	<div class="wrap w-full h-auto flex flex-col items-center justify-center py-4">
+		<h1 class="text-neutral text-4xl my-12">About us</h1>
+		<div class="admins grid grid-cols-1 grid-rows-4">
+			{#each adminsConfig as admin}
+				<UserCard info={admin} />
+			{/each}
+		</div>
+		<div class="container-md m-auto h-auto others mt-4 px-4">
+			<h1 class="text-4xl font-normal text-center">Others</h1>
+			<div class="grid grid-cols-4 grid-rows-10 md:grid-cols-8 md:grid-rows-4 gap-2 mt-2">
+				{#each usersConfig as user}
+					<Member ing={user} />
+				{/each}
+			</div>
+		</div>
+	</div>
+</div>
---
+++ src/lib/components/home/Footer.svelte
@@ -0,0 +1,46 @@
+<script>
+	import DiscordIcon from '../icons/DiscordIcon.svelte';
+	import FacebookIcon from '../icons/FacebookIcon.svelte';
+	import GithubIcon from '../icons/GithubIcon.svelte';
+</script>
+
+<footer class="bg-base-100 text-base-content">
+	<div class="divider"></div>
+	<div class="p-10 footer wrap max-w-5xl m-auto">
+		<div>
+			<h1 class="text-xl font-bold text-primary">Programming PH</h1>
+			<p>
+				Developed and maintained by the contributors.
+			</p>
+		</div>
+		<div>
+			<span class="footer-title">SOCIALS</span>
+			<div class="flex flex-col items-start justify-center gap-2 text-primary">
+				<a
+					href="https://discord.gg/MmWwgXQezf"
+					class="flex items-center justify-start gap-2"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<DiscordIcon className="w-4 h-4" /> Discord
+				</a>
+				<a
+					href="https://github.com/programmingphilippines"
+					class="flex items-center justify-start gap-2"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<GithubIcon className="w-4 h-4" /> Github
+				</a>
+				<a
+					href="https://www.facebook.com/groups/programmingphilippines"
+					class="flex items-center justify-start gap-2"
+					rel="noreferrer"
+					target="_blank"
+				>
+					<FacebookIcon className="w-4 h-4" /> Facebook
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
