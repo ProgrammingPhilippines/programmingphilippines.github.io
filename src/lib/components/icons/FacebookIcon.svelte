b1f59e44b258bf005e6fffe8f57e319b6162d126
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:16:52 +0800

    add: icons

---
+++ src/lib/components/icons/FacebookIcon.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let className = 'w-6 h-6';
+</script>
+
+<svg
+	width="200px"
+	height="200px"
+	viewBox="0 0 16 16"
+	xmlns="http://www.w3.org/2000/svg"
+	fill="#000000"
+	class={className}
+	><g id="SVGRepo_bgCarrier" stroke-width="0" /><g id="SVGRepo_iconCarrier"
+		><path
+			fill="#000000"
+			d="M15 8a7 7 0 00-7-7 7 7 0 00-1.094 13.915v-4.892H5.13V8h1.777V6.458c0-1.754 1.045-2.724 2.644-2.724.766 0 1.567.137 1.567.137v1.723h-.883c-.87 0-1.14.54-1.14 1.093V8h1.941l-.31 2.023H9.094v4.892A7.001 7.001 0 0015 8z"
+		/></g
+	></svg
+>
