f96e38b8bd8eee131c49bf412779541eea9ef64a
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:17:11 +0800

    add: tooltip and card

---
+++ src/lib/components/cards/Member.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let ing: string = 'anonymous';
+</script>
+
+<div class="tooltip tooltip-top" data-tip={ing}>
+	<div class="btn btn-circle btn-lg">
+		<div class="avatar placeholder">
+			<div class="bg-neutral-focus text-neutral-content rounded-full w-16">
+				<!-- <img
+					class="w-full h-full"
+					src="https://avatars.githubusercontent.com/u/46004116?v=4"
+					alt="avatar"
+				/> -->
+				<span class="text-xl">
+					{ing[0].toUpperCase()}
+				</span>
+			</div>
+		</div>
+	</div>
+</div>
