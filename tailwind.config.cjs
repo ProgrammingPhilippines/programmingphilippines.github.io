70cb08f1bae2fb4904555bf487c90008a0389239
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:16:17 +0800

    add: daisyui theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
+	content: ["./src/**/*.{html,js,svelte,ts}"],
+	theme: {
+		extend: {},
+	},
+	daisyui: {
+		themes: [
+			{
+				pphTheme: {
+					primary: "#EA0747",
+					secondary: "#E4EA67",
+					accent: "#D8F49C",
+					neutral: "#18151E",
+					"base-100": "#F5EFF6",
+					info: "#2E84E0",
+					success: "#2CE2CD",
+					warning: "#FAB647",
+					error: "#F7625F",
+				},
+			},
+		],
+	},
+	plugins: [require("daisyui")],
+};
