2a53f3dac20f7078e7c47902a5e1e812e33a233f
mystique09 <knightkrypto7@gmail.com>
2022-12-31 00:16:47 +0800

    add: config

---
+++ src/lib/config/admins.ts
@@ -0,0 +1,38 @@
+export const adminsConfig: CardInfo[] = [
+	{
+		name: 'Gervin Guevarra',
+		position: 'Senior Software Engineer',
+		image: 'gerv_admin.png',
+		description: `
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Donec et risus ac turpis interdum facilisis eget et justo. Mauris nec ex pellentesque, mattis magna a, venenatis dui. Sed venenatis ex ac mauris vehicula, eu scelerisque dolor blandit.
+		`,
+		flip: false,
+	},
+	{
+		name: 'Kyle Cedric',
+		position: 'Senior Software Engineer',
+		image: 'gerv_admin.png',
+		description: `
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Donec et risus ac turpis interdum facilisis eget et justo. Mauris nec ex pellentesque, mattis magna a, venenatis dui. Sed venenatis ex ac mauris vehicula, eu scelerisque dolor blandit.
+		`,
+		flip: true,
+	},
+	{
+		name: 'Ivan Ilagan',
+		position: 'Senior Software Engineer',
+		image: 'gerv_admin.png',
+		description: `
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Donec et risus ac turpis interdum facilisis eget et justo. Mauris nec ex pellentesque, mattis magna a, venenatis dui. Sed venenatis ex ac mauris vehicula, eu scelerisque dolor blandit.
+		`,
+		flip: false,
+	},
+	{
+		name: 'Derrick',
+		position: 'Senior System Administrator',
+		image: 'gerv_admin.png',
+		description: `
+		Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Donec et risus ac turpis interdum facilisis eget et justo. Mauris nec ex pellentesque, mattis magna a, venenatis dui. Sed venenatis ex ac mauris vehicula, eu scelerisque dolor blandit.
+		`,
+		flip: true,
+	},
+];
---
+++ src/lib/config/users.ts
@@ -0,0 +1,12 @@
+export const usersConfig: string[] = [
+	"Benjie Ben Garcia",
+	"Cunning Bard",
+	"John Carlo Camara",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+	"Benjie Ben Garcia",
+];
