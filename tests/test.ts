e4ffca5b67bb526fc97efad75af792bd8d3ee198
mystique09 <knightkrypto7@gmail.com>
2022-12-31 17:57:22 +0800

    add: main test

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,14 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+test("main page", async ({ page }) => {
+	await page.goto("/");
+	expect(
+		await page.isVisible(
+			"body > div > div.navbar.duration-150.max-w-4xl.m-auto > div:nth-child(1)",
+		),
+	).toBe(true);
+	expect(await page.isVisible("svg")).toBe(true);
+	expect(await page.isVisible("a")).toBe(true);
+	expect(await page.isVisible(".navbar")).toBe(true);
+	expect(await page.textContent("#about-us > div > h1")).toBe("About us");
 });
