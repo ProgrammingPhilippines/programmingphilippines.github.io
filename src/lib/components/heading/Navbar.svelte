6c268ce75f41340d50ed58d93ae8236402a5c560
mystique09 <knightkrypto7@gmail.com>
2022-12-31 15:48:50 +0800

    add: type, and external field

--- src/lib/components/heading/Navbar.svelte
+++ src/lib/components/heading/Navbar.svelte
@@ -1,14 +1,23 @@
-<script>
+<script lang="ts">
 	import { slide } from 'svelte/transition';
 	import Logo from '../icons/Logo.svelte';
 	import MenuIcon from '../icons/MenuIcon.svelte';
 	import XMark from '../icons/XMark.svelte';
 
-	const navLinks = [
-		{ href: '/', label: 'Home' },
-		{ href: '/#about-us', label: 'About us' },
-		{ href: '/community-projects', label: 'Community projects' },
-		{ href: '/events', label: 'Events' }
+	type NavLink = {
+		href: string;
+		label: string;
+		external: boolean;
+	};
+
+	const navLinks: NavLink[] = [
+		{ href: '/#about-us', label: 'About us', external: false },
+		{
+			href: 'https://github.com/programmingphilippines',
+			label: 'Community projects',
+			external: true
+		},
+		{ href: '/events', label: 'Events', external: false }
 	];
 
 	let isOpen = false;
@@ -19,13 +28,21 @@
 
 <div class="navbar duration-150 max-w-4xl m-auto">
 	<div class="navbar-start">
-		<Logo className="w-16 h-16" />
+		<a href="/">
+			<Logo className="w-16 h-16" />
+		</a>
 	</div>
 	<ul
-		class="hidden md:flex md:items-center md:justify-center md:gap-8 md:w-full md:navbar-center font-normal"
+		class="hidden md:flex md:items-center md:justify-center md:gap-8 md:w-full md:navbar-end font-normal"
 	>
 		{#each navLinks as link}
-			<li class="hover:scale-110 duration-100"><a href={link.href}>{link.label}</a></li>
+			<li class="hover:scale-110 duration-100">
+				{#if link.external}
+					<a href={link.href} target="_blank" rel="noreferrer">{link.label}</a>
+				{:else}
+					<a href={link.href}>{link.label}</a>
+				{/if}
+			</li>
 		{/each}
 	</ul>
 	<div class="navbar-end flex w-full items-center justify-end md:hidden">
@@ -37,17 +54,21 @@
 		<div
 			in:slide={{ duration: 300 }}
 			out:slide={{ duration: 200 }}
-			class="container bg-base-200 fixed z-10 top-0 left-0 h-screen"
+			class="bg-base-200 fixed z-10 top-0 left-0 h-screen w-screen"
 		>
-			<div class="close-btn absolute top-0 right-0 flex items-center justify-center">
+			<div class="close-btn absolute top-4 right-2 flex items-center justify-center">
 				<button on:click={toggleMenu} class="btn btn-circle btn-ghost">
 					<XMark className="w-6 h-6 stroke-primary" />
 				</button>
 			</div>
-			<ul class="navbar-center w-full flex flex-col items-center gap-4">
+			<ul class="w-full flex flex-col items-center gap-8">
 				{#each navLinks as link}
 					<li on:keydown={toggleMenu} on:click={toggleMenu}>
-						<a href={link.href}>{link.label}</a>
+						{#if link.external}
+							<a href={link.href} target="_blank" rel="noreferrer">{link.label}</a>
+						{:else}
+							<a href={link.href}>{link.label}</a>
+						{/if}
 					</li>
 				{/each}
 			</ul>
