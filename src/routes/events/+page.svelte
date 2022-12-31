65617f5373e0a7205bf18fde4315207ed7c7f408
mystique09 <knightkrypto7@gmail.com>
2022-12-31 15:48:20 +0800

    add: events

---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	type GroupEvent = {
+		name: string;
+		description: string;
+	};
+
+	const events: GroupEvent[] = [
+		{
+			name: 'DAFAQ session #1',
+			description: 'DAFAQ is software engineering?',
+		},
+		{
+			name: 'DAFAQ session #2',
+			description: 'DAFAQ is software testing?',
+		},
+		{
+			name: 'DAFAQ session #3',
+			description: 'DAFAQ Mathematics?',
+		},
+		{
+			name: 'DAFAQ session #4',
+			description: 'DAFAQ Game development?',
+		},
+		{
+			name: 'DAFAQ session #5',
+			description: 'DAFAQ Git/Github?',
+		},
+	];
+</script>
+
+<div class="min-h-screen">
+	<div class="conatiner max-w-4xl m-auto p-4">
+		<h1 class="text-4xl text-center">EVENTS!</h1>
+		<span class="mt-2">Da Frequently Ask Questions</span>
+		<ul class="steps steps-vertical w-full h-full">
+			{#each events as event}
+				<li class="step step-primary w-full p-2">
+					<p class="tooltip tooltip-bottom md:tooltip-right" data-tip={event.description}>{event.name}</p>
+				</li>
+			{/each}
+			<li class="step step-neutral w-full p-2">
+				<p class="tooltip tooltip-bottom md:tooltip-right" data-tip="Coming soon!">Coming soon!</p>
+			</li>
+		</ul>
+	</div>
+</div>
