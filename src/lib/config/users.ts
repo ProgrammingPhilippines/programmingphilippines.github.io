de4dff31879b52425d0e5104566d40a0a777d51b
mystique09 <knightkrypto7@gmail.com>
2022-12-31 15:48:32 +0800

    add: githubId

--- src/lib/config/users.ts
+++ src/lib/config/users.ts
@@ -3,16 +3,52 @@
 export const usersConfig: MemberInfo[] = [
 	{
 		name: 'Benjie Ben Garcia',
-		githubNodeId: '50514928',
+		githubId: '50514928',
 		githubUsername: 'mystique09'
 	},
 	{
 		name: 'Cunning Bard',
-		githubNodeId: '61928294',
+		githubId: '61928294',
 		githubUsername: 'CunningBard'
 	},
 	{
 		name: 'John Carlo Camara',
-		githubNodeId: '61928294',
+		githubId: '61928294',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
+	},
+	{
+		name: 'Anonymous',
+		githubId: '00000000',
 	},
 ]
